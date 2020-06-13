97a8bfd46792614e903d3bd824b952ebc83600c7
Karen Matala <karenirene.cano1203@gmail.com>
2020-06-13 20:25:51 +0800

    Create function calling by handleFunc

--- main.go
+++ main.go
@@ -12,7 +12,12 @@ func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(f.Message))
 }
 
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("bar called"))
+}
+
 func main() {
 	http.Handle("/foo", &fooHandler{Message: "hello worldz"})
+	http.HandleFunc("/", barHandler)
 	http.ListenAndServe(":5000", nil)
 }
