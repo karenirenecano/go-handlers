c23dcaff873656ae09ef4d48214307fb830c37e7
Karen Matala <karenirene.cano1203@gmail.com>
2020-06-14 15:03:22 +0800

    Add CORS on middleware

--- main.go
+++ main.go
@@ -163,6 +163,9 @@ func middlewareHandler(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("before handler; middleware start")
 		start := time.Now()
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Add("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization, X-CSRF-Token, Accept-Encoding")
 		handler.ServeHTTP(w, r)
 		fmt.Printf("middleware finished; %s", time.Since(start))
 	})
